f9922125983b98033189787f5f1248863624c396
Gaurav Jain <gauravjain0781@gmail.com>
2023-09-26 08:54:32 +0530

    Cart Page Completed with required JS

--- script.js
+++ script.js
@@ -14,10 +14,33 @@ if (close) {
     })
 }
 
-let price = document.getElementsByClassName('price').value;
-let quantity = document.getElementsByClassName('quantity').value;
-console.log(typeof(quantity));
+let totalElement = document.getElementsByClassName('total');
 
-function calculate() {
+function total() {
+    let price = Array.from(document.getElementsByClassName('price'));
+    let quantity = Array.from(document.getElementsByClassName('quantity'));
+
+    for(let i = 0; i < price.length; i++){            
+        let p = parseInt(price[i].textContent);
+        let q = parseInt(quantity[i].value);
+        totalElement[i].innerHTML = "Rs. " + p*q;
+    }
+}      
+
+function calculate_subtotal() {
+    let subtotal = 0;
+
+    for(let i = 0 ; i < totalElement.length; i++){
+        subtotal = subtotal + parseInt(totalElement[i].textContent.replace(/\D/g, ''));
+    }
     
+    let cart_total = subtotal + parseInt(document.getElementById('shipping_charges').textContent);
+    
+    document.getElementById('sub_total').innerHTML = subtotal;
+    document.getElementById('cart_total').innerHTML = cart_total;
+}
+
+window.onload = function() {
+    total();
+    calculate_subtotal();
 }
