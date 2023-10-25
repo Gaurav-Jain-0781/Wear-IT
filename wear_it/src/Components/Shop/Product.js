98e185a126c886d7ac28f718a1085ebdeb70e067
Gaurav Jain <gauravjain0781@gmail.com>
2023-10-25 21:51:18 +0530

    Added Dynamic Functionality to Shop Page Buttons

--- wear_it/src/Components/Shop/Product.js
+++ wear_it/src/Components/Shop/Product.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductList from '../Home/Product-List'
 import { Products } from './Data'
-import { FaArrowRight } from 'react-icons/fa'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 
 const Product = () => {
   const [page, setPage] = useState(1)
   const productsPerPage = 10;
   const startIndex = (page - 1) * productsPerPage;
   const endIndex = startIndex + productsPerPage;
+
+  useEffect(() => {    
+    const buttons = document.querySelectorAll('#pagination button')
+
+    buttons.forEach((button) => {
+      const pagebutton = parseInt(button.textContent)
+
+      if(pagebutton ===  page){
+        button.classList.add('active')
+      }
+      else{
+        button.classList.remove('active')
+      }
+    })
+  }, [page])
 
   return (
     <>
@@ -19,8 +34,13 @@ const Product = () => {
         </div>
       </section>
       <section id="pagination" className="indexing">
-        <button onClick={(e) => setPage(parseInt(e.target.textContent))}> 1 </button>
-        <button onClick={(e) => setPage(parseInt(e.target.textContent))}> 2 </button>
+        <button onClick={() => setPage(page-1)}><FaArrowLeft/></button>
+        {Array
+        .from({length : Math.ceil(Products.length / productsPerPage)})
+        .fill(null)
+        .map((_, i) => {
+          return <button key={i} onClick={(e) => setPage(parseInt(e.target.textContent))}>{i+1}</button>
+        })} 
         <button onClick={() => setPage(page+1)}><FaArrowRight/></button>
     </section>
     </>
